281af49d2749820b02c96390b8e9caa8178c56ad
edmand46 <theedison4@gmail.com>
2026-03-26 16:03:52 +0300

    fix: tests.yml

---
+++ testdata/lua/sample.go
@@ -0,0 +1,39 @@
+package lua
+
+type Vector3 struct {
+	X float32 `pack:"min=-500,max=500,bits=16"`
+	Y float32 `pack:"min=-500,max=500,bits=16"`
+	Z float32 `pack:"min=-500,max=500,bits=16"`
+}
+
+type Opcode uint16
+
+const (
+	OpcodeUnknown Opcode = iota
+	OpcodeAuthorize
+	OpcodeJoinRoom
+)
+
+type MoveMessage struct {
+	Position  Vector3
+	Velocity  [3]float32
+	Waypoints []Vector3
+	PlayerID  uint32
+	Active    bool
+	Visible   bool
+	Ghost     bool
+	Name      string
+}
+
+type SpawnMessage struct {
+	EntityID uint32
+	Position Vector3
+	Health   int16
+	Tags     []string
+	Data     []uint8
+}
+
+type EnvelopeMessage struct {
+	Code    Opcode
+	Counter uint8
+}
